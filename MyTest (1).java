362aeea4bcad18e8b57891c2b9f7beffc46617da
treex2 <92065249+treex2@users.noreply.github.com>
2022-02-08 15:05:59 -0600

    Add files via upload

---
+++ MyTest (1).java
@@ -0,0 +1,268 @@
+package org.firstinspires.ftc.teamcode.Manual;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Main Drive", group = "")
+public class MyTest extends LinearOpMode {
+		//All code here has been optimized for human readability, this means the following has been done...
+		//
+		//Comments added everywhere. If something doesn't make sense, read those
+		//Anything that needs multiple lines has been split up very clearly to indicate differences
+		//Anything that made no sense to have as multiple lines has been simplified to 1, but has an in-depth comment about how it works
+		//
+		//If you would like the shortened version, let me know, and I can cut all the empty space, and make it illegible
+
+		public static final double motorStr = -1; //Default motor power
+		public static float multiplier = 1; //Multiplier to apply to motors, can actually be changed during runtime
+		public static float deadSpace = 0.08f; //Amount of deadspace on joystick (not counted for movement)
+		private DcMotor spinner; //Thing
+		private DcMotor frontLeft; //Front left
+		private DcMotor backLeft; //Back left
+		private DcMotor backRight; //Back right
+		private DcMotor frontRight; //Front right
+		private Servo grabber_servo; //hand servo
+		private DcMotor arm; //Arm
+
+		@Override
+		public void runOpMode() {
+				spinner = hardwareMap.dcMotor.get("spin");
+				frontLeft = hardwareMap.dcMotor.get("front_left");
+				backLeft = hardwareMap.dcMotor.get("back_left");
+				backRight = hardwareMap.dcMotor.get("back_right");
+				frontRight = hardwareMap.dcMotor.get("front_right");
+				grabber_servo = hardwareMap.servo.get("grabber");
+				arm = hardwareMap.dcMotor.get("arm");
+				
+				boolean handOpen = false;
+				boolean changedHand = false;
+				boolean changedSpd = false;
+				boolean changedArmPos = false;
+				int adjustArmBy = -1200;
+				//Init. goes here, above start
+				//arm.setPower(.1);
+				//sleep(1000);
+				grabber_servo.setPosition(-10);
+				
+				arm.setPower(-.33);
+				sleep(1400);
+				arm.setPower(0);
+				arm.setTargetPosition(0);
+				adjustArmBy = -arm.getCurrentPosition();
+				arm.setPower(.23);
+				sleep(1150);
+				int calcPos = arm.getCurrentPosition() + adjustArmBy;
+				arm.setPower((1000-calcPos)*.00013f);
+				/*while(!opModeIsActive())
+				{
+					int armPos = arm.getCurrentPosition();
+					int calcPos = armPos + adjustArmBy;
+					arm.setPower((1000-calcPos)*.00013f);
+				}*/
+				waitForStart();
+				while (opModeIsActive()) {
+						//Look, I didn't put this here, but if it works, who am I to say no?
+						//I suspect it's to adjust for 2 being in the opposite direction as the other 2 (opposite orientations)
+						spinner.setDirection(DcMotorSimple.Direction.REVERSE);
+						frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+						backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+						// telemetry.update(); // If code is broken, try uncommenting this.
+						int armPos = arm.getCurrentPosition();
+						// Controls for NOT driving controller...
+
+						// Spin the spinner if the x or y button is pressed
+						
+						if(gamepad2.x)
+							spinner.setPower(.45);
+						else if(gamepad2.y)
+							spinner.setPower(-.45);
+						else
+							spinner.setPower(0);
+						//spinner.setPower(gamepad2.x ? (.75 : gamdpad2.y ? (-.75 : 0)));
+						
+						//Set one grab button, press once to close, once to open
+						if((gamepad2.a || gamepad2.b) && !handOpen && !changedHand) { //open
+								grabber_servo.setPosition(-10);
+								handOpen = true;
+								changedHand = true;
+						}
+						else if((gamepad2.b || gamepad2.a) && handOpen && !changedHand) { //close
+								grabber_servo.setPosition(200);
+								handOpen = false;
+								changedHand = true;
+						}
+						if(!gamepad2.b && !gamepad2.a)
+							changedHand = false;
+						
+						
+						if(gamepad2.start && !changedArmPos)
+						{
+							changedArmPos = true;
+							adjustArmBy = -armPos;
+						}
+						else	
+							changedArmPos = false;
+						//telemetry.addData("Hand Open? ",handOpen);
+						//telemetry.update();
+						
+						
+						//if(gamepad2.a)
+						//		grabber_servo.setPosition(grabber_servo.getPosition() < 0 ? (100 : -10));
+
+						//#region Original Code
+						/*if (gamepad2.a)
+								grabber_servo.setPosition(-10);
+						else if (gamepad2.b)
+								grabber_servo.setPosition(100);*/
+						//#endregion
+
+						//If the left bumper is pressed or the joystick is up, set the power to .3,
+						//else if the right bumper is pressed or the joystick is down, set the power to -.3,
+						//else if the position is forward (not leaning towards bot) set the power to .05, to prevent it from falling to the ground.
+						//=-=-=-=-=-=-=-=-=-=-=-=
+						//SEE ARM.GETPOSITION()
+						//arm.setPower((gamepad2.left_bumper || gamepad2.left_stick_y < 0) ?? (.3 : (gamepad2.right_bumper || gamepad2.left_stick_y > 0) ?? (-.3 : arm.getPosition() > 0 ? (.05 : 0)));
+
+						//#region Origianl Code
+						
+						
+						// arm controls up / down
+						
+						// armPos == -1550 at ground
+						// armPos == -550 at highest point
+						calcPos = armPos + adjustArmBy;
+						
+						if ((gamepad2.left_bumper || gamepad2.left_stick_y < 0 || gamepad2.dpad_up) && calcPos < 1300)// && (armPos < 1010+adjustArmBy || gamepad2.b))
+						{
+							//
+							if(grabber_servo.getPosition() < 75) //OPEN
+								arm.setPower(0.50);
+							else if(grabber_servo.getPosition() > 75) //CLOSED
+								arm.setPower(0.75);
+						}
+						else if ((gamepad2.right_bumper || gamepad2.left_stick_y > 0 || gamepad2.dpad_down))// && (armPos > 160+adjustArmBy || gamepad2.b))
+						{
+							//
+							arm.setPower(-.4);
+						}
+						else
+						{
+								//arm.setPower(.1);
+								//arm.setPower((armPos+adjustArmBy)/5312.5f);
+							arm.setPower((1000-calcPos)*.00013f);
+						}
+						
+						telemetry.addData("Arm Position:", armPos+adjustArmBy);
+						telemetry.update();
+						
+
+
+						//Controls for driving controller
+
+						//Do we want logic for modifying the deadZone for control(s)?
+						//I'd have to do a little more work to implement it, so decided not to actually do anything until idea is approved/disapproved
+
+						//If the multiplier hasn't been changed...
+						//If the dpad is down, decrease mult by .25
+						//Else if the dpad is up, increase mult by .25
+						//Else increase by 0
+						//Else increase by 0 (duplicate intentional)
+						if(!changedSpd) {
+							if(gamepad1.dpad_down || gamepad1.left_bumper) {
+								multiplier -= .25;
+								changedSpd = true;
+							}
+							else if(gamepad1.dpad_up || gamepad1.right_bumper) {
+								multiplier += .25;
+								changedSpd = true;
+							}
+						}
+						
+						if(changedSpd && !gamepad1.dpad_down && !gamepad1.dpad_up) {
+							changedSpd = false;
+						}
+						//multipler += !changedSpd ? (gamepad1.dpad_down ? (-.25f : gamepad1.dpad_up ? (.25f : 0)) : 0);
+
+						//Get the distance from the center of the left joystick. Let me know if we want right one, also.
+						float yDif = Math.abs(gamepad1.left_stick_y); //Distance from y = 0
+						if(yDif < deadSpace) yDif = 0;
+						//yDif = yDif > deadSpace ? (yDif : 0); //If within deadspace, set calculation distance to 0
+						float xDif = Math.abs(gamepad1.left_stick_x); //Distance from x = 0
+						if(xDif < deadSpace) xDif = 0;
+						//xDif = xDif > deadSpace ? (xDif : 0); //If within deadspace, set calculation distance to 0
+
+						//Y pos < 0 is back/down
+						//Y pos > 0 is forward/up
+						//X pos > 0 is right
+						//X pos < 0 is left
+
+						// + power == motor turning forward
+						// - power == motor turning backwards
+						double power = motorStr * multiplier;
+						
+						//telemetry.addData("key1", power);
+						
+						if (gamepad1.left_stick_y < deadSpace && yDif > xDif) {
+								//BACKWARD
+								telemetry.addData("key1", -power);
+								backRight.setPower(power);
+								frontLeft.setPower(power); //Faulty motor
+								frontRight.setPower(-power*.7);
+								backLeft.setPower(power);
+						}
+						else if (gamepad1.left_stick_y > deadSpace && yDif > xDif) {
+								//FORWARD
+								telemetry.addData("key1", power);
+								backRight.setPower(-power);
+								frontLeft.setPower(-power); //Faulty motor
+								frontRight.setPower(power*.7);
+								backLeft.setPower(-power);
+						}
+						else if (gamepad1.left_stick_x > deadSpace && xDif > yDif) {
+								//STRAFE RIGHT
+								backRight.setPower(power);
+								frontLeft.setPower(-power); //Faulty motor
+								frontRight.setPower(-power);
+								backLeft.setPower(-power);
+						}
+						else if (gamepad1.left_stick_x < deadSpace && xDif > yDif)
+						{
+								//STRAFE LEFT
+								backRight.setPower(-power);
+								frontLeft.setPower(power); //Faulty motor
+								frontRight.setPower(power);
+								backLeft.setPower(power);
+						}
+						else if (gamepad1.right_stick_x > 0 || gamepad1.right_trigger > 0) //Right joystick control (changed from trigger)
+						{
+								//TURN RIGHT
+								frontRight.setPower(power*0.5); //Faulty motor
+								backLeft.setPower(-power*.5);
+								frontLeft.setPower(power*.5);
+								backRight.setPower(power*.5);
+						}
+						else if (gamepad1.right_stick_x < 0 || gamepad1.left_trigger > 0) //Right joystick control (changed from trigger)
+						{
+								//TURN LEFT
+								frontRight.setPower(-power*.5); //Faulty motor
+								backRight.setPower(-power*.5);
+								backLeft.setPower(power*.5);
+								frontLeft.setPower(-power*.5);
+						}
+						else
+						{
+								//STOP
+								backRight.setPower(0);
+								frontRight.setPower(0);
+								backLeft.setPower(0);
+								frontLeft.setPower(0);
+						}
+						//telemetry.addData("key2", gamepad1.right_stick_x);
+					//telemetry.update();
+				}
+		}
+}
